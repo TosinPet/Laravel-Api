70908d97c2381675fb1dd85f6ddf4d18e47e7640
Adeniyi Peter <adeniyipeteroluwatosin@gmail.com>
2023-11-24 06:07:28 +0100

    latest changes

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,5 +1,5 @@
 function customer_deposit(utilized_credit, credit_limit, credit_allowance){
-    alert('Baluga')
+    // alert('Baluga')
         document.getElementById('utilized_credit').innerHTML = utilized_credit
         document.getElementById('credit_limit').innerHTML = credit_limit
         document.getElementById('credit_allowance').innerHTML = credit_allowance
