41c3a711b35416ca7a6d2f0b940313d6a8b675c7
Adeniyi Peter <adeniyipeteroluwatosin@gmail.com>
2024-01-11 16:39:40 +0100

    fixes

---
+++ database/migrations/2024_01_11_153840_add_utilized_credit_to_customers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            //
+            $table->integer('utilized_credit')->after('reference_no')->nullable();
+            $table->integer('credit_limit')->after('utilized_credit')->nullable();
+            $table->integer('credit_allowance')->after('credit_limit')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            //
+        });
+    }
+};
