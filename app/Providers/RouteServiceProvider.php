28b561c503b75c3dbc7d11369d9dd5f06523e902
Adeniyi Peter <adeniyipeteroluwatosin@gmail.com>
2024-01-15 11:13:37 +0100

    fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,8 +35,8 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
-
-            Route::prefix('admin')
+            
+            Route::middleware('admin')
                 ->group(base_path('routes/admin.php'));
         });
     }
